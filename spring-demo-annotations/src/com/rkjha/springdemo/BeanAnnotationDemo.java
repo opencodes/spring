088aed9bdde83194b8d82254fe92543ea65fb39c
Rajesh Jha <rkjha@Rajeshs-MacBook-Pro.local>
2021-01-17 17:33:35 +0530

    No XML Configuration

--- spring-demo-annotations/src/com/rkjha/springdemo/BeanAnnotationDemo.java
+++ spring-demo-annotations/src/com/rkjha/springdemo/BeanAnnotationDemo.java
@@ -1,5 +1,6 @@
 package com.rkjha.springdemo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
@@ -7,8 +8,8 @@ public class BeanAnnotationDemo {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
         Coach coach = context.getBean("tennisCoach", Coach.class);
 
         System.out.println(coach.getDailyWorkout());
